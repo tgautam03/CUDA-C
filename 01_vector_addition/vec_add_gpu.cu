34da363069577910eb21013bb5042b9dd391175b
tgautam03 <30533136+tgautam03@users.noreply.github.com>
2024-06-22 19:33:10 -0600

    Added CUDA CHECK macro

--- 01_vector_addition/vec_add_gpu.cu
+++ 01_vector_addition/vec_add_gpu.cu
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define CUDA_CHECK(err) {if (err != cudaSuccess){printf("%s in %s at line %d \n", cudaGetErrorString(err), __FILE__, __LINE__);exit(EXIT_FAILURE);}}
+
 __global__ void vec_add_kernel(float* A, float* B, float* C, int N)
 {
     int i = blockDim.x*blockIdx.x + threadIdx.x;
@@ -19,48 +21,24 @@ void vec_add_gpu(float* A, float* B, float* C, int N)
 
     // Device memory allocation
     cudaError_t err_A = cudaMalloc((void**) &d_A, N*sizeof(float));
-    if (err_A != cudaSuccess)
-    {
-        printf("%s in %s at line %d \n", cudaGetErrorString(err_A), __FILE__, __LINE__);
-        exit(EXIT_FAILURE);
-    } 
+    CUDA_CHECK(err_A);
     cudaError_t err_B = cudaMalloc((void**) &d_B, N*sizeof(float));
-    if (err_B != cudaSuccess)
-    {
-        printf("%s in %s at line %d \n", cudaGetErrorString(err_B), __FILE__, __LINE__);
-        exit(EXIT_FAILURE);
-    } 
+    CUDA_CHECK(err_B); 
     cudaError_t err_C = cudaMalloc((void**) &d_C, N*sizeof(float));
-    if (err_C != cudaSuccess)
-    {
-        printf("%s in %s at line %d \n", cudaGetErrorString(err_C), __FILE__, __LINE__);
-        exit(EXIT_FAILURE);
-    } 
+    CUDA_CHECK(err_C);
 
     // Copying A and B to device memory
     cudaError_t err_A_ = cudaMemcpy(d_A, A, N*sizeof(float), cudaMemcpyHostToDevice);
-    if (err_A_ != cudaSuccess)
-    {
-        printf("%s in %s at line %d \n", cudaGetErrorString(err_A_), __FILE__, __LINE__);
-        exit(EXIT_FAILURE);
-    } 
+    CUDA_CHECK(err_A_);
     cudaError_t err_B_ = cudaMemcpy(d_B, B, N*sizeof(float), cudaMemcpyHostToDevice);
-    if (err_B_ != cudaSuccess)
-    {
-        printf("%s in %s at line %d \n", cudaGetErrorString(err_B_), __FILE__, __LINE__);
-        exit(EXIT_FAILURE);
-    } 
+    CUDA_CHECK(err_B_);
 
     // Kernel execution
     vec_add_kernel<<<ceil(N/256.0), 256>>>(d_A, d_B, d_C, N);
 
     // Copy back results from device to host
     cudaError_t err_C_ = cudaMemcpy(C, d_C, N*sizeof(float), cudaMemcpyDeviceToHost);
-    if (err_C_ != cudaSuccess)
-    {
-        printf("%s in %s at line %d \n", cudaGetErrorString(err_C_), __FILE__, __LINE__);
-        exit(EXIT_FAILURE);
-    }
+    CUDA_CHECK(err_C_);
 
     // Free device memory
     cudaFree(d_A);
